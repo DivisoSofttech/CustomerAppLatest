23d1ddfc9ed41f02f8911485fd67cea8980d3a82
jishnulxi <jishnu.t@lxisoft.com>
2019-12-13 09:39:16 +0530

    Added release.build.android.sh

--- release.build.android.sh
+++ release.build.android.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ionic cordova build android --prod --release
 cd /Users/jishnuj/Desktop/work/CustomerAppLatest/platforms/android/app/build/outputs/apk/release/
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore /Users/jishnuj/Desktop/work/CustomerAppLatest/foodexp.pkcs12 app-release-unsigned.apk foodexp.ie
 /Users/jishnuj/Library/Android/sdk/build-tools/28.0.3/zipalign -v 4 app-release-unsigned.apk foodexp-release.apk
