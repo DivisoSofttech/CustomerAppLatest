24f813846aa37aaab060b6bf87629f0a95a817a4
jishnulxi <jishnu.t@lxisoft.com>
2020-01-14 16:45:33 +0530

    Preorder/Collection Time Selection Fix

--- src/app/components/preorder/preorder.component.ts
+++ src/app/components/preorder/preorder.component.ts
@@ -14,17 +14,15 @@ import { Util } from 'src/app/services/util';
 })
 export class PreorderComponent implements OnInit {
 
-  fromTime;
-  toTime;
+  openingTime;
+  closingTime;
   selectedTime;
 
   @Input() isCollection = false;
 
   @Input() store: Store;
 
-  hourArray = [];
-
-  minuteArray = [];
+  closeTommorow = false;
 
   constructor(
     private popoverController: PopoverController,
@@ -35,63 +33,71 @@ export class PreorderComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-
-    this.logger.info(this, '>>>>>>>>>>>FromTime', this.store.openingTime, ' toTime>>>>>>>>>>>', this.store.closingTime);
-
-    let ftime = moment(this.store.openingTime);
-    let ttime = moment(this.store.closingTime);
-
-    this.fromTime = ftime.format("hh:mm a")
-    this.toTime = ttime.format("hh:mm a");
-    this.logger.info(this, 'From Time>>>>>', this.fromTime, ' toTime>>>>>>', this.toTime);
+    this.openingTime = moment(this.store.openingTime).format('hh:mm A');
+    this.closingTime = moment(this.store.closingTime).format('hh:mm A')
+    this.checkIfStoreClosesTommorow();
   }
 
-  dismissTrue() {
-    const date = new Date().toLocaleDateString();
-    let ftime = moment(this.store.openingTime).format('hh:mm a');
-    let ttime = moment(this.store.closingTime).format('hh:mm a');
-    let stime = moment(this.selectedTime).format('hh:mm a');
+  checkIfStoreClosesTommorow() {
+    let openingDate = new Date(this.store.openingTime);
+    let closingDate = new Date(this.store.closingTime);
+    let fromDate = new Date();
+    let toDate = new Date();
+    fromDate.setHours(openingDate.getHours());
+    fromDate.setMinutes(openingDate.getMinutes());
+    toDate.setHours(closingDate.getHours());
+    toDate.setMinutes(closingDate.getMinutes());
+    let momentOpeningDate = moment(fromDate);
+    let momentClosingDate = moment(toDate);
+    if (momentClosingDate.isBefore(momentOpeningDate)) {
+      this.logger.info(this, 'Adding one Day to closing date');
+      this.closeTommorow = true;
+      console.error(this.closeTommorow);
+      toDate.setDate(toDate.getDate() + 1);
+      momentClosingDate = moment(toDate);
+    }
+  }
 
-    let fdate = moment(date + ' ' + ftime);
-    let tdate = moment(date + ' ' + ttime);
-    let sdate = moment(date + ' ' + stime);
+  checkSelectedDate() {
+    let openingDate = new Date(this.store.openingTime);
+    let closingDate = new Date(this.store.closingTime);
+    let selectedDate = new Date(this.selectedTime);
+    let fromDate = new Date();
+    let toDate = new Date();
+    let sDate = new Date();
 
-    if (tdate.isBefore(fdate)) {
-      this.logger.info(this, 'Adding One day to Closing Time')
-      tdate.add(1,'day');
-      if (sdate.isBefore(fdate)) {
-        this.logger.info(this, 'Adding One day to Selected Time')
-        sdate.add(1,'day');
+    fromDate.setHours(openingDate.getHours());
+    fromDate.setMinutes(openingDate.getMinutes());
+    toDate.setHours(closingDate.getHours());
+    toDate.setMinutes(closingDate.getMinutes());
+    sDate.setHours(selectedDate.getHours());
+    sDate.setMinutes(selectedDate.getMinutes());
+
+    let momentOpeningDate = moment(fromDate);
+    let momentClosingDate = moment(toDate);
+    let momentSelectedDate = moment(sDate);
+
+    if (momentClosingDate.isBefore(momentOpeningDate)) {
+      this.logger.info(this, 'Adding one Day to closing date');
+      this.closeTommorow = true;
+      toDate.setDate(toDate.getDate() + 1);
+      momentClosingDate = moment(toDate);
+      if (momentSelectedDate.isBefore(momentOpeningDate)) {
+        sDate.setDate(sDate.getDate() + 1);
+        momentSelectedDate = moment(sDate);
       }
     }
-
-    this.selectedTime = stime;
- 
-    if (sdate.isAfter(fdate) && sdate.isBefore(tdate)) {
-      const tempTime = sdate;
-      this.logger.info(this, 'Seleceted Time After Conversion', tempTime.toDate());
-      this.cartService.preOrderDate = tempTime.toDate().toISOString();
+    if (momentSelectedDate.isBetween(momentOpeningDate, momentClosingDate)) {
+      this.cartService.preOrderDate = momentSelectedDate.toISOString();
       this.popoverController.dismiss(true);
     } else {
-      this.util.createToast('Select a Time Between ' + `${this.fromTime}` + ' to ' + `${this.toTime}`)
+      this.util.createToast('Select a Time Between ' + `${this.openingTime}` + ' to ' + `${this.closingTime}`)
     }
-
   }
 
-  convert12to24(time12h) {
-    const [time, modifier] = time12h.split(' ');
 
-    let [hours, minutes] = time.split(':');
-
-    if (hours === '12') {
-      hours = '00';
-    }
-
-    if (modifier === 'PM') {
-      hours = parseInt(hours, 10) + 12;
-    }
-
-    return `${hours}:${minutes}`;
+  dismissTrue() {
+    this.checkSelectedDate();
   }
 
   dismiss() {
